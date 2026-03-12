f898a2c90577f37abca18ec933946ec838b3a917
André Ricardo Fazanaro Martinon <martinon@gmail.com>
2026-03-12 11:17:30 -0300

    CLI tool user documentation.

--- src/iono_scint_map/cli.py
+++ src/iono_scint_map/cli.py
@@ -164,10 +164,11 @@ def cli():
     msg = _("This is free software; see the source code for copying conditions."
             " There is ABSOLUTELY NO WARRANTY; not even for MERCHANTABILITY or "
             "FITNESS FOR A PARTICULAR PURPOSE. For details, type "
-            "'iono_scint_map show'.\n\nReference the paper 'A new approach for "
-            "the generation of real-time GNSS low-latitude ionospheric "
-            "scintillation maps' when using the software in academic papers, "
-            "thesis etc. <https://doi.org/10.1051/swsc/2023015>")
+            "'iono-scint-map show'.\n\nPlease reference the paper 'A new "
+            "approach for the generation of real-time GNSS low-latitude "
+            "ionospheric scintillation maps' when using the software for "
+            "academic work (publications, thesis etc). "
+            "<https://doi.org/10.1051/swsc/2023015>")
 
     console = Console(width=CONSOLE_WIDTH, highlight=False)
 
@@ -186,18 +187,21 @@ def cli():
 
 @cli.command('show')
 def show_warranty_information() -> None:
-    msg_warranty_1 = _('IONO_SCINT_MAP is free software: you can redistribute '
+    """Show software licensing information.
+    """
+
+    msg_warranty_1 = _('IONO-SCINT-MAP is free software: you can redistribute '
                        'it and/or modify it under the terms of the GNU General '
                        'Public License as published by the Free Software '
                        'Foundation, either version 3 of the License, or (at '
                        'your option) any later version.')
-    msg_warranty_2 = _('IONO_SCINT_MAP is distributed in the hope that it will '
+    msg_warranty_2 = _('IONO-SCINT-MAP is distributed in the hope that it will '
                        'be useful, but WITHOUT ANY WARRANTY; without even the '
                        'implied warranty of MERCHANTABILITY or FITNESS FOR A '
                        'PARTICULAR PURPOSE. See the GNU General Public License '
                        'for more details.')
     msg_warranty_3 = _('You should have received a copy of the GNU General '
-                       'Public License along with IONO_SCINT_MAP; see the file '
+                       'Public License along with IONO-SCINT-MAP; see the file '
                        'COPYING. If not, see <https://www.gnu.org/licenses/>.')
 
     console = Console(width=CONSOLE_WIDTH)
@@ -223,44 +227,79 @@ def show_warranty_information() -> None:
      click.FloatRange(-180.0, 180.0),
      click.FloatRange(-90.0, 90.0),
      click.FloatRange(-90.0, 90.0)]),
-              default=[-78.0, -30.0, -39.0, 9.0], show_default=True)
+              default=[-78.0, -30.0, -39.0, 9.0], show_default=True,
+              help=_('Set the boundary box of the interpolation grid.'))
 # Elevation cut-off
-@click.option('-e', '--elevation', type=click.FloatRange(
-    0.0, 90.0), default=30.0, show_default=True)
+@click.option('-e', '--elevation',
+              type=click.FloatRange(0.0, 90.0), default=30.0,
+              show_default=True,
+              help=_('Set the minimum elevation angle (degrees) between the '
+                     'receiver and the satellite for the scintillation index '
+                     'to be considered valid.'))
 # Constellations
 @click.option('-c', '--constellation',
               type=click.Choice(Constellation, case_sensitive=False),
               default=[Constellation.GPS, Constellation.GLONASS,
                        Constellation.GALILEO, Constellation.BEIDOU],
-              show_default=True, multiple=True)
+              show_default=True, multiple=True,
+              help=_('Choose the GNSS constellations to be considered when '
+                     'filtering scintillation data set.'))
 # Preprocessing options
 @click.option('-p', '--preprocessing',
               type=click.Choice(PreprocessingOptions, case_sensitive=False),
-              default=PreprocessingOptions.VQI, show_default=True)
+              default=PreprocessingOptions.VQI, show_default=True,
+              help=_('Choose a preprocessing option for grouping and '
+                     'aggregating IPPs: '
+                     'SAI (Slant S4 values reduced by the average value at the '
+                     'centroid); SMI (Slant S4 values reduced by the maximum '
+                     'value at the centroid); SQI (Slant S4 values reduced by '
+                     'the average value above the 3rd quartile at the centroid)'
+                     '; VAI (Vertical S4 values reduced by the average value '
+                     'at the centroid); VMI (Vertical S4 values reduced by the '
+                     'maximum value at the centroid); VQI (Vertical S4 values '
+                     'reduced by the average value above the 3rd quartile at '
+                     'the centroid)'))
 # Interpolation options
 @click.option('-i', '--interpolation',
               type=click.Choice(InterpolationOptions, case_sensitive=False),
-              default=InterpolationOptions.GPR, show_default=True)
+              default=InterpolationOptions.GPR, show_default=True,
+              help=_('Choose an interpolation algorithm: '
+                     'GDA (Scipy GridData); '
+                     'GPR (Gaussian Process Regression); '
+                     'IDW (Inverse Distance Weighting); '
+                     'RBF (Radial Basis Functions)'))
 # Interpolation options
 @click.option('--interpolation-grid-resolution',
               type=click.FLOAT,
-              default=0.25, show_default=True)
+              default=0.25, show_default=True,
+              help=_('Set the resolution (lat x lon, degrees) of the '
+                     'interpolation grid. '))
 # Interpolation options
 @click.option('--ipp-group-resolution',
               type=click.FLOAT,
-              default=1.0, show_default=True)
+              default=1.0, show_default=True,
+              help=_('Set the resolution (lat x lon, degrees) of the IPPs '
+                     'grouping and aggregation cell.'))
 # Remove stations
 @click.option('-r', '--remove-station',
               type=click.STRING,
-              default=None, show_default=True, multiple=True)
+              default=None, show_default=True, multiple=True,
+              help=_('Remove all the GNSS station data from the scintillation '
+                     'data set. Inform each ones you want to remove.'))
 # Default p
 @click.option('--default-p',
               type=click.FLOAT,
-              default=2.6, show_default=True)
+              default=2.6, show_default=True,
+              help=_('Spectral slope of detrended phase in the 0.1 to 25Hz '
+                     'range'))
 @click.option('--start',
-              type=click.DateTime(formats=['%Y-%m-%dT%H:%M:%S']), default=None)
+              type=click.DateTime(formats=['%Y-%m-%dT%H:%M:%S']), default=None,
+              help=_('Specify the start time to filter the scintillation data '
+                     'set.'))
 @click.option('--end',
-              type=click.DateTime(['%Y-%m-%dT%H:%M:%S']), default=None)
+              type=click.DateTime(['%Y-%m-%dT%H:%M:%S']), default=None,
+              help=_('Specify the end time to filter the scintillation data '
+                     'set.'))
 # Scintillation data file
 @click.argument('scint_data_file',
                 type=click.Path(exists=True, path_type=Path, resolve_path=True))
@@ -271,7 +310,7 @@ def show_warranty_information() -> None:
 @click.argument('output_file',
                 type=click.Path(dir_okay=True,
                                 path_type=Path,
-                                resolve_path=True), default='scint_map.hdf5')
+                                resolve_path=True), default='scint_map.h5')
 def create_scint_map(scint_index: ScintillationIndex,
                      extent: Tuple[float, float, float, float],
                      elevation: float,
@@ -287,14 +326,23 @@ def create_scint_map(scint_index: ScintillationIndex,
                      scint_data_file: Path,
                      gnss_stations_file: Path,
                      output_file: Path):
-    """Create an ionospheric scintillation map
+    """Create an ionospheric scintillation map.
 
-    Parameters
+    Arguments:
 
     SCINT_DATA_FILE: file path
 
         The scintillation data file path. Only CSV and PARQUET formats are
         accepted.
+
+    GNSS_STATIONS_FILE: file path
+
+        The GNSS stations information data file path. Only CSV and PARQUET
+        formats are accepted.
+
+    OUTPUT_FILE: file path (Default: ./scint_map.h5)
+
+        The file path for the output interpolated map data in HDF5 format.
     """
 
     scint_map_data = ScintillationMapDataset(
@@ -337,35 +385,55 @@ def create_scint_map(scint_index: ScintillationIndex,
 @click.argument('scint_map_file',
                 type=click.Path(exists=True, path_type=Path, resolve_path=True))
 @click.option('--dip/--no-dip', is_flag=True, default=True,
-              show_default=True)
+              show_default=True,
+              help=_('Choose to plot, or not, the magnetic latitudes. The '
+                     'magnetic inclination is calculated using the IGRF model '
+                     'valid for the date range of the scintillation data set.'))
 @click.option('--png/--no-png', is_flag=True, default=True,
-              show_default=True)
+              show_default=True,
+              help=_('Choose to save, or not, the resulting plot in the PNG '
+                     'format.'))
 @click.option('--pdf/--no-pdf', is_flag=True, default=True,
-              show_default=True)
+              show_default=True,
+              help=_('Choose to save, or not, the resulting plot in the PDF '
+                     'format.'))
 @click.option('--dpi', type=click.IntRange(72, 1200),
-              default=300, show_default=True)
+              default=300, show_default=True,
+              help=_('Set the DPI (Dots Per Inch) resolution of the resulting '
+                     'PNG image file.'))
 @click.option('--stations/--no-stations', is_flag=True,
-              default=True, show_default=True)
+              default=True, show_default=True,
+              help=_('Choose to plot, or not, a marker for each GNSS station '
+                     'location'))
 @click.option('--axis/--no-axis', is_flag=True,
-              default=True, show_default=True)
+              default=True, show_default=True,
+              help=_('Choose to plot, or not, the Longitude and Latitude '
+                     'axes.'))
 @click.option('--clipping/--no-clipping', is_flag=True,
-              default=False, show_default=True)
+              default=False, show_default=True,
+              help=_('Choose to clipping, or not, the map to the area '
+                     'delimited by the convex hull of all IPPs.'))
 @click.option('--convex-hull/--no-convex-hull', is_flag=True,
-              default=False, show_default=True)
+              default=False, show_default=True,
+              help=_('Choose to plot, or not, the convex hull polygon.'))
 @click.option('--map-extent', nargs=4, type=click.Tuple(
     [click.FloatRange(-180.0, 180.0),
      click.FloatRange(-180.0, 180.0),
      click.FloatRange(-90.0, 90.0),
      click.FloatRange(-90.0, 90.0)]),
-              default=[-81.0, -27.0, -39.0, 11.0], show_default=True)
-@click.option('--igrf-extent', nargs=4, type=click.Tuple(
+              default=[-81.0, -27.0, -39.0, 11.0], show_default=True,
+              help=_('Set the boundary box used to plot the map.'))
+@click.option('--dip-extent', nargs=4, type=click.Tuple(
     [click.FloatRange(-180.0, 180.0),
      click.FloatRange(-180.0, 180.0),
      click.FloatRange(-90.0, 90.0),
      click.FloatRange(-90.0, 90.0)]),
-              default=[-81.0, -27.0, -45.0, 15.0], show_default=True)
+              default=[-81.0, -27.0, -45.0, 15.0], show_default=True,
+              help=_('Set the boundary box used to plot the magnetic '
+                     'latitudes.'))
 @click.option('--output-dir', type=click.Path(
-    exists=True, path_type=Path, resolve_path=True))
+    exists=True, path_type=Path, resolve_path=True),
+              help=_('Set the output directory path. Default: ./'))
 def plot_scint_map(scint_map_file: Path,
                    dip: bool,
                    png: bool,
@@ -376,8 +444,17 @@ def plot_scint_map(scint_map_file: Path,
                    clipping: bool,
                    convex_hull: bool,
                    map_extent: Tuple[float, float, float, float],
-                   igrf_extent: Tuple[float, float, float, float],
+                   dip_extent: Tuple[float, float, float, float],
                    output_dir: str):
+    """Plot an ionospheric scintillation map.
+
+    Arguments:
+
+    SCINT_MAP_FILE: file path
+
+        The interpolated scintillation map data file path. Only HDF5 files
+        generated by the iono-scint-map create command is accepted.
+    """
 
     scint_map_data = ScintillationMapDataset.from_hdf5(scint_map_file)
 
@@ -400,7 +477,7 @@ def plot_scint_map(scint_map_file: Path,
     if dip:
         plot_igrf(ax,
                   scint_map_data.start_timestamp,
-                  extent=igrf_extent,
+                  extent=dip_extent,
                   color='black',
                   fontsize=18)
     if axis:
@@ -431,34 +508,52 @@ def plot_scint_map(scint_map_file: Path,
 @click.argument('scint_map_file',
                 type=click.Path(exists=True, path_type=Path, resolve_path=True))
 @click.option('--dip/--no-dip', is_flag=True, default=True,
-              show_default=True)
+              show_default=True,
+              help=_('Choose to plot, or not, the magnetic latitudes. The '
+                     'magnetic inclination is calculated using the IGRF model '
+                     'valid for the date range of the scintillation data set.'))
 @click.option('--png/--no-png', is_flag=True, default=True,
-              show_default=True)
+              show_default=True,
+              help=_('Choose to save, or not, the resulting plot in the PNG '
+                     'format.'))
 @click.option('--pdf/--no-pdf', is_flag=True, default=True,
-              show_default=True)
+              show_default=True,
+              help=_('Choose to save, or not, the resulting plot in the PDF '
+                     'format.'))
 @click.option('--dpi', type=click.IntRange(72, 1200),
-              default=300, show_default=True)
+              default=300, show_default=True,
+              help=_('Set the DPI (Dots Per Inch) resolution of the resulting '
+                     'PNG image file.'))
 @click.option('--stations/--no-stations', is_flag=True,
-              default=True, show_default=True)
+              default=True, show_default=True,
+              help=_('Choose to plot, or not, a marker for each GNSS station '
+                     'location'))
 @click.option('--agg/--no-agg', is_flag=True, default=False,
-              show_default=True)
+              show_default=True,
+              help=_('Choose between show the grouped and aggregated IPPs or '
+                     'all the original IPPs before grouping and aggregation.'))
 @click.option('--map-extent', nargs=4, type=click.Tuple(
     [click.FloatRange(-180.0, 180.0),
      click.FloatRange(-180.0, 180.0),
      click.FloatRange(-90.0, 90.0),
      click.FloatRange(-90.0, 90.0)]),
-              default=[-81.0, -27.0, -39.0, 11.0], show_default=True)
-@click.option('--igrf-extent', nargs=4, type=click.Tuple(
+              default=[-81.0, -27.0, -39.0, 11.0], show_default=True,
+              help=_('Set the boundary box used to plot the map.'))
+@click.option('--dip-extent', nargs=4, type=click.Tuple(
     [click.FloatRange(-180.0, 180.0),
      click.FloatRange(-180.0, 180.0),
      click.FloatRange(-90.0, 90.0),
      click.FloatRange(-90.0, 90.0)]),
-              default=[-81.0, -27.0, -45.0, 15.0], show_default=True)
+              default=[-81.0, -27.0, -45.0, 15.0], show_default=True,
+              help=_('Set the boundary box used to plot the magnetic '
+                     'latitudes.'))
 @click.option('--size',
               type=click.Choice([c**2 for c in range(1, 15)]),
-              default=64, show_default=True)
+              default=64, show_default=True,
+              help=_('Set the marker size of each plotted IPP.'))
 @click.option('--output-dir', type=click.Path(
-    exists=True, path_type=Path, resolve_path=True))
+    exists=True, path_type=Path, resolve_path=True),
+              help=_('Set the output directory path. Default: ./'))
 def plot_scint_map(scint_map_file: Path,
                    dip: bool,
                    png: bool,
@@ -467,9 +562,18 @@ def plot_scint_map(scint_map_file: Path,
                    stations: bool,
                    agg: bool,
                    map_extent: Tuple[float, float, float, float],
-                   igrf_extent: Tuple[float, float, float, float],
+                   dip_extent: Tuple[float, float, float, float],
                    size: int,
                    output_dir: str):
+    """Plot an IPP (Ionospheric Pierce Points) samples map.
+
+    Arguments:
+
+    SCINT_MAP_FILE: file path
+
+        The interpolated scintillation map data file path. Only HDF5 files
+        generated by the iono-scint-map create command is accepted.
+    """
 
     scint_map_data = ScintillationMapDataset.from_hdf5(scint_map_file)
 
@@ -480,7 +584,7 @@ def plot_scint_map(scint_map_file: Path,
     if dip:
         plot_igrf(ax,
                   scint_map_data.start_timestamp,
-                  extent=igrf_extent,
+                  extent=dip_extent,
                   color='black',
                   fontsize=18)
 
